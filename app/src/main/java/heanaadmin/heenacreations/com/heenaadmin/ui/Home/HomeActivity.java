0f3755f1c79020f1c043856c193f4306dbe68528
pradeep <pradeepemc3@gmail.com>
2019-03-16 20:28:25 +0530

    QR code scan implementation

--- app/src/main/java/heanaadmin/heenacreations/com/heenaadmin/ui/Home/HomeActivity.java
+++ app/src/main/java/heanaadmin/heenacreations/com/heenaadmin/ui/Home/HomeActivity.java
@@ -1,12 +1,18 @@
 package heanaadmin.heenacreations.com.heenaadmin.ui.Home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import heanaadmin.heenacreations.com.heenaadmin.R;
 import heanaadmin.heenacreations.com.heenaadmin.ui.BaseActivity;
@@ -16,23 +22,33 @@ import heanaadmin.heenacreations.com.heenaadmin.ui.fragments.OrderFragment;
 public class HomeActivity extends BaseActivity {
 
     private ActionBar mToolBar;
+    //qr code scanner object
+    private IntentIntegrator qrScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        toolbar();
         init();
+    }
+
+    /**
+     * Configure toolbar
+     */
+    private void toolbar() {
+        mToolBar = getSupportActionBar();
+        mToolBar.setTitle("Employees");
+        loadFragment(new EmployeesFragment());
+        BottomNavigationView navigationView = findViewById(R.id.navigation);
+        navigationView.setOnNavigationItemSelectedListener(mNavigationSelectListener);
     }
 
     /**
      * UI Initialization
      */
     private void init() {
-        mToolBar = getSupportActionBar();
-        mToolBar.setTitle("Employees");
-        loadFragment(new EmployeesFragment());
-        BottomNavigationView navigationView = findViewById(R.id.navigation);
-        navigationView.setOnNavigationItemSelectedListener(mNavigationSelectListener);
+        qrScan = new IntentIntegrator(this);
     }
 
     /**
@@ -75,5 +91,37 @@ public class HomeActivity extends BaseActivity {
         transaction.replace(R.id.frame_container, fragment);
         transaction.addToBackStack(null);
         transaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_items, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.qr_code_scan:
+                // to open scanner
+                qrScan.initiateScan();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned Data:: "+result.getContents(), Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
