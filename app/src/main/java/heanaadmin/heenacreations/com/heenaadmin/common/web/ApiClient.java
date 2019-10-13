5304185b350afa8aada7564a7d209e995ac34670
Arati-P <aratipailwan@gmail.com>
2019-10-13 21:22:22 +0530

    Same as previous

--- app/src/main/java/heanaadmin/heenacreations/com/heenaadmin/common/web/ApiClient.java
+++ app/src/main/java/heanaadmin/heenacreations/com/heenaadmin/common/web/ApiClient.java
@@ -9,7 +9,6 @@ public class ApiClient {
     public static final String BASE_URL = "";
     private static Retrofit retrofit = null;
 
-
     public static Retrofit getClient() {
         if (retrofit==null) {
             retrofit = new Retrofit.Builder()
