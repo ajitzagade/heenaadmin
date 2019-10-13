08ba05637071dff021f91fb6258d4a5a77e85840
Arati-P <aratipailwan@gmail.com>
2019-10-13 22:02:55 +0530

    1.Added web Apiclient and request and response model 2.Done changes in register and login UI

--- app/src/main/java/heanaadmin/heenacreations/com/heenaadmin/common/web/ApiClient.java
+++ app/src/main/java/heanaadmin/heenacreations/com/heenaadmin/common/web/ApiClient.java
@@ -4,6 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
+
     public static final String BASE_URL = "";
     private static Retrofit retrofit = null;
 
